80ccbf20db489f90350ade7d84f16d543dbd626f
jianglanni <72094879+jianglanni@users.noreply.github.com>
2020-11-08 23:13:07 +0800

    Add files via upload
    basically finished phase 2, not tested yet.

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -6,55 +6,79 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-#include "queue.h"
 #include "private.h"
 #include "uthread.h"
+#include "queue.h"
+
+uthread_ctx_t main_ctx; //Where we keep the ctx for main thread.
+
+queue_t tcb_queue = NULL; //where we keep the TCBs
+
+int thread_is_exit = 0; //Modified to 1 when some thread calls exit()
 
 struct uthread_tcb {
-    // Need default values;
-    void *sp; //stack pointer;
-    uthread_ctx_t *context;
-    int state;
+	/* TODO Phase 2 */
+	int state; //0 for ready, 1 for running, 2 for blocked.
+	uthread_ctx_t* ctx;
+	void *top_of_stack;
 };
+
+struct uthread_tcb* current_running;
 
 struct uthread_tcb *uthread_current(void)
 {
-    return current_thread;
+	/* TODO Phase 2 */
+	return current_running;
 }
 
 void uthread_yield(void)
 {
-
+	/* TODO Phase 2 */
+	queue_enqueue(tcb_queue, (void*) uthread_current());
+	uthread_ctx_switch(current_running -> ctx, main_ctx); //Go back to main thread.
 }
 
 void uthread_exit(void)
 {
-
+	/* TODO Phase 2 */
+	thread_is_exit = 1; //Let main thread know that there is an exit.
+	uthread_ctx_switch(current_running -> ctx, main_ctx);
 }
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-    struct *uthread_tcb newThread = (uthread_tcb*) malloc(sizeof(uthread_tcb));
-    if(newThread == NULL){
-        return -1;
-    }
-    newThread->sp = uthread_ctx_alloc_stack();
-    uthread_ctx_init(newThread->context, newThread->sp, func, arg);
-    newThread->state = 0;
-    return 0;
+	struct uthread_tcb *created_thread = NULL;
+
+	created_thread = (struct uthread_tcb*) malloc(sizeof(struct uthread_tcb)); //Allocate memory space for TCB.
+	if (created_thread == NULL)
+		return -1;
+
+	uthread_ctx_alloc_stack(created_thread -> top_of_stack); //Handle the stack.
+
+	created_thread -> ctx = malloc(sizeof(uthread_ctx_t));
+	uthread_ctx_init(created_thread -> ctx, created_thread -> top_of_stack, func, arg);
+	
+	queue_enqueue(tcb_queue, (void*) created_thread); //Add the created TCB into the global queue. 
 }
 
 int uthread_start(uthread_func_t func, void *arg)
 {
-    struct *uthread_tcb newThread = (uthread_tcb*) malloc(sizeof(uthread_tcb));
-    if(newThread == NULL){
-        return -1;
-    }
-    newThread->sp = uthread_ctx_alloc_stack();
-    uthread_ctx_init(newThread->context, newThread->sp, func, arg);
-    newThread->state = 0;
-
-    return 0;
+	/* TODO Phase 2 */
+	tcb_queue = queue_create();
+	uthread_create(func, arg);
+	while (queue_length(tcb_queue)) {
+		queue_dequeue(tcb_queue, &current_running);
+		current_running -> state = 1;
+		uthread_ctx_switch(main_ctx, current_running -> ctx); //Jump to the scheduled func
+		// ** The boi is running... and done! ** //
+		if (thread_is_exit) { //Exit is called
+			uthread_ctx_destroy_stack(current_running -> top_of_stack);
+			free(current_running);
+			thread_is_exit = 0; //reset the signal.
+		}
+	}
+	queue_destroy(tcb_queue);
+	tcb_queue = NULL; //Reset
 }
 
 void uthread_block(void)
@@ -66,3 +90,4 @@ void uthread_unblock(struct uthread_tcb *uthread)
 {
 	/* TODO Phase 2/3 */
 }
+
