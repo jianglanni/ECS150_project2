8e0acac1c2829697b3a0211e2790ce69ef7e14f6
Manan Shah <46585928+mkshah98@users.noreply.github.com>
2020-11-09 22:52:24 +0530

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -6,17 +6,28 @@
 
 struct semaphore {
 	int count;
-	bool semUnlocked;
+    queue_t waitList;   //has the list of threads waiting for a resource.
+    int isLocked; // set to 1 if a sem is locked
 };
+
+/*
+ * thread asking for or releasing resource.
+ * It will be set to blocked state if sem count is 0
+ */
+//struct uthread_tcb asking_thread;
 
 sem_t sem_create(size_t count)
 {
     sem_t *new_sem;
-    *new_sem = (sem_t) malloc(sizeof(struct semaphore));
-    if (new_sem != NULL) {
-        new_sem -> count  = count;
-        new_sem->semUnlocked = 1;
+    *new_sem = *(sem_t) malloc(sizeof(struct semaphore));
+    if (new_sem == NULL){
+        return 0;
     }
+
+    new_sem->count  = count;
+    new_sem->waitlist = queue_create();
+    new_sem->isLocked = 0;
+
     return *new_sem;
 }
 
@@ -26,7 +37,10 @@ int sem_destroy(sem_t sem)
         return -1;
 	}
 	sem->count = 0;
+	sem->isLocked = 0;
+	queue_destroy(sem->waitList);
 	free(sem);
+
 	return 0;
 }
 
@@ -35,22 +49,35 @@ int sem_down(sem_t sem)
     if(sem == NULL){
         return -1;
     }
-    sem->count = sem->count - 1;
-    while(sem->count < 0){
-        //block process;
+
+    while(sem->count == 0){
+        uthread_block();
+        queue_enqueue(sem->waitlist,uthread_current());
     }
+
+    uthread_unblock(uthread_current());
+    queue_dequeue(sem->waitList,&uthread_current());
+    sem->count -= 1;
+    sem->isLocked = 1;
+
     return 0;
 }
 
 int sem_up(sem_t sem)
 {
-	if(sem == NULL){
+    struct uthread_tcb* curr = uthread_current();
+    if(sem == NULL){
         return -1;
-	}
-	sem->count += 1;
-	while (sem->count <= 0){
-	    //block process
-	}
-	return 0;
-}
+    }
 
+    if(sem->count == 0){
+        if(queue_length(sem->waitList)){
+            uthread_unblock(curr);
+            queue_dequeue(sem->waitList, &uthread_current());
+        }
+        sem->count += 1;
+        sem->isLocked = 0;
+    }
+
+    return 0;
+}
